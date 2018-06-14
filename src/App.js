4f91a5e61ecbb3da6e7e105d1ba5ed0c703db446
Vizena2005 <vizena2005@gmail.com>
2018-06-14 12:17:51 -0500

    Starting new repo with file structure for react complete. SCSS ported over already from my static portfolio.

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import NavTabs from "./components/NavTabs";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import About from "./components/pages/Work";
+
+const App = () => (
+  <Router>
+    <div>
+      <NavTabs />
+      <Route exact path="/work" component={Work} />
+      <Route exact path="/about" component={About} />
+      <Route path="/" component={Home} />
+    </div>
+  </Router>
+);
+
+export default App;
